47853720c516bd3b149d5a69a29fbcc46e683963
Dhruv Unde <75470798+dhruvcode413@users.noreply.github.com>
2021-06-30 12:00:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import os
-os.system("conda install portaudio")
-os.system("conda install pyaudio")
-os.system("pip install -r C:\\Users\\dhruv\\Documents\\Projects\\Friday\\requirements.txt")
-os.system
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -16,14 +12,10 @@ import tkinter
 import json
 import random
 import operator
-import winshell
-import feedparser
 import ctypes
 import requests
 import shutil
-from clint.textui import progress
 from bs4 import BeautifulSoup
-import win32com.client as wincl
 from urllib.request import urlopen
 import time
 
@@ -51,7 +43,7 @@ def takeCommand():
 
     except Exception as e:
         with open(".Errors.log", 'a') as f:
-            f.write(e+'\n') 
+            f.write(str(e)+'\n') 
         speak("Say that again please...")  
         return "None"
     return query
@@ -150,6 +142,7 @@ if __name__ == "__main__":
         
         elif 'execute' in query:
             query = query.replace('open', '')
+            query = query.replace('execute', '')
             speak('OK, executing '+query)
             os.system(query)
 
@@ -176,48 +169,18 @@ if __name__ == "__main__":
                 result += query[count] + ' '
                 count += 1
             if 'command' in result:
-                os.system("cmd.exe")
-            
-            elif 'code' in result:
-                os.system("code")
+                os.system("bash")
 
             elif 'browser' in result:
-                os.startfile("C:\\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
-            
-            elif 'sublime' in result:
-                os.startfile("C:\\Program Files (x86)\Sublime Text 3\sublime_text.exe")
-            
-            elif 'visual studio' in result:
-                os.startfile("C:\\Program Files (x86)\Microsoft Visual Studio\\2019\Community\Common7\IDE\devenv.exe")
+                os.system("firefox")
             
             elif 'github' in result:
-                os.startfile("C:\\Users\dhruv\AppData\Local\GitHubDesktop\GitHubDesktop.exe")
-            
-            elif "zoom" in result:
-                os.startfile("C:\\Users\dhruv\AppData\Roaming\Zoom\\bin\Zoom.exe")
+                os.system("github-dektop")
 
 
         elif 'joke' in query:
             speak(pyjokes.get_joke())
-        elif 'news' in query:
-             
-            try:
-                jsonObj = urlopen('''https://newsapi.org / v1 / articles?source = the-times-of-india&sortBy = top&apiKey =\\46f67483cd6a40a8a86ffc784bdb6fdd\\''')
-                data = json.load(jsonObj)
-                i = 1
-                 
-                speak('here are some top news from the times of india')
-                print('''=============== TIMES OF INDIA ============'''+ '\n')
-                 
-                for item in data['articles']:
-                     
-                    print(str(i) + '. ' + item['title'] + '\n')
-                    print(item['description'] + '\n')
-                    speak(str(i) + '. ' + item['title'] + '\n')
-                    i += 1
-            except Exception as e:
-                 
-                print(str(e))
+        
         elif "calculate" in query:
              
             app_id = "G7RL9K-3PRAHV2PA8"
@@ -226,36 +189,17 @@ if __name__ == "__main__":
             query = query.split()[indx + 1:]
             res = client.query(' '.join(query))
             answer = next(res.results).text
-            print("The answer is " + answer)
             speak("The answer is " + answer)
-            
-        elif 'lock' in query:
-            speak("locking the device")
-            ctypes.windll.user32.LockWorkStation()
  
         elif 'shutdown system' in query:
             speak("Hold On a Sec ! Your system is on its way to shut down")
-            subprocess.call('shutdown / p /f')
-                 
-        elif 'empty recycle bin' in query:
-            winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
-            speak("Recycle Bin Recycled")
+            subprocess.call('shutdown now')
         
         elif "where is" in query:
             query = query.replace("where is", "")
             location = query
-            speak("User asked to Locate")
-            speak(location)
+            speak("Here are the result")
             web.open("https://www.google.nl / maps / place/" + location + "")
         
         elif "restart" in query:
-            subprocess.call(["shutdown", "/r"])
-             
-        elif "hibernate" in query or "sleep" in query:
-            speak("Hibernating")
-            subprocess.call("shutdown / h")
- 
-        elif "log out" in query or "sign out" in query:
-            speak("Make sure all the application are closed before sign-out")
-            time.sleep(5)
-            subprocess.call(["shutdown", "/l"])
+            os.system("reboot")
